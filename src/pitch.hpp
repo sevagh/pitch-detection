746de309cfcb0979182c76988ea9d46b9100592d
Sevag Hanssian <sevag.hanssian@gmail.com>
2016-12-21 09:23:34 -0500

    remove mentions of goertzel/dft

--- src/pitch.hpp
+++ src/pitch.hpp
@@ -4,22 +4,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
-#include "pitch.hpp"
-#include "mpm.hpp"
-#include "goertzel.hpp"
-#include "dft.hpp"
-#include "yin.hpp"
-#include "autocorrelation.hpp"
+#include <pitch.hpp>
+#include <mpm.hpp>
+#include <yin.hpp>
+#include <autocorrelation.hpp>
 
 
 double get_pitch(std::string algo, std::vector<double> data, int sample_rate)
 {
 	if (algo == "mpm") {
 		return get_pitch_mpm(data, sample_rate);
-	} else if (algo == "goertzel") {
-		return get_pitch_goertzel(data, sample_rate);
-	} else if (algo == "dft") {
-		return get_pitch_dft(data, sample_rate);
 	} else if (algo == "autocorrelation") {
 		return get_pitch_autocorrelation(data, sample_rate);
 	} else if (algo == "yin") {
