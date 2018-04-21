68061be70eb213429fd98c5ddd6bd406d85cf77e
Sevag Hanssian <sevag.hanssian@gmail.com>
2018-04-21 13:19:51 -0700

    C interface to mpm

--- include/pitch_detection.h
+++ include/pitch_detection.h
@@ -15,4 +15,9 @@ get_pitch_goertzel(const std::vector<double> &audio_buffer, int sample_rate);
 double
 get_pitch_dft(const std::vector<double> &audio_buffer, int sample_rate);
 
+extern "C" {
+double
+get_pitch_mpm_c(const double *arr, int size, int sample_rate);
+}
+
 #endif /* PITCH_DETECTION_H */
