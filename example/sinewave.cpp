b4b629f10feb47a05a28cac0b0607f426e78d953
Sevag Hanssian <sevag.hanssian@gmail.com>
2018-10-13 20:21:47 -0400

    Add option to plot with lags

--- example/sinewave.cpp
+++ example/sinewave.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
+#include <float.h>
 #include <fstream>
 #include <functional>
 #include <gflags/gflags.h>
@@ -42,15 +43,16 @@ DEFINE_validator(algo, [](const char *flagname, const std::string &value) {
 	return false;
 });
 
-DEFINE_uint64(size, 4096, "Sine wave size (single channel)");
-DEFINE_validator(size, [](const char *flagname, uint64_t value) {
+DEFINE_int32(size, 4096, "Sine wave size (single channel)");
+DEFINE_validator(size, [](const char *flagname, int value) {
 	if (value >= 0)
 		return true;
 
 	return false;
 });
 
-DEFINE_bool(plot, false, "Output sinewave data to stderr");
+DEFINE_bool(plot, false, "Output sinewave data to stdout");
+DEFINE_bool(plot_lags, false, "Output sinewave data with lags to stdout");
 DEFINE_bool(quiet, false, "Suppress most outputs");
 
 static std::vector<double>
@@ -66,11 +68,46 @@ main(int argc, char **argv)
 	auto data = generate_sinewave(
 	    2 * FLAGS_size, FLAGS_frequency, FLAGS_sample_rate, FLAGS_noise);
 
-	if (FLAGS_plot)
-		for (auto x: data)
-			std::cout << x << std::endl;
+	if (FLAGS_plot) {
+		if (FLAGS_plot_lags) {
+			std::cerr << "--plot and --plot_lags are mutually exclusive"
+			          << ::std::endl;
 
-	if (!FLAGS_quiet && (FLAGS_algo == "mpm") && !(FLAGS_size && !(FLAGS_size & (FLAGS_size - 1))))
+			return -1;
+		}
+
+		for (auto x : data)
+			std::cout << x << std::endl;
+	}
+
+	if (FLAGS_plot_lags) {
+		std::vector<double> orig(2 * FLAGS_size);
+		std::vector<double> quarter_lag(2 * FLAGS_size);
+		std::vector<double> half_lag(2 * FLAGS_size);
+		std::vector<double> full_lag(2 * FLAGS_size);
+
+		for (int i = 0; i < 2 * FLAGS_size; ++i) {
+			if (i < FLAGS_size)
+				orig[i] = data[i];
+			if ((i > FLAGS_size / 4) && (i < FLAGS_size / 4 + FLAGS_size)) {
+				quarter_lag[i] = data[i - FLAGS_size / 4];
+			}
+			if ((i > FLAGS_size / 2) && (i < FLAGS_size / 2 + FLAGS_size)) {
+				half_lag[i] = data[i - FLAGS_size / 2];
+			}
+			if (i > FLAGS_size) {
+				full_lag[i] = data[i - FLAGS_size];
+			}
+		}
+
+		for (int i = 0; i < 2 * FLAGS_size; ++i) {
+			std::cout << orig[i] << "\t" << quarter_lag[i] << "\t"
+			          << half_lag[i] << "\t" << full_lag[i] << std::endl;
+		}
+	}
+
+	if (!FLAGS_quiet && (FLAGS_algo == "mpm") &&
+	    !(FLAGS_size && !(FLAGS_size & (FLAGS_size - 1))))
 		std::cerr << "FFTS performs better with power-of-two sizes"
 		          << std::endl;
 
@@ -90,12 +127,12 @@ generate_sinewave(
 
 	std::random_device rnd_device;
 	std::mt19937 mersenne_engine{rnd_device()}; // Generates random integers
-	std::uniform_int_distribution<int> dist{0, int(size / 2) - 1};
+	std::uniform_int_distribution<int> dist(0, int(size / 2) - 1);
 
 	auto gen = [&dist, &mersenne_engine]() { return dist(mersenne_engine); };
 
 	std::vector<int> elements_to_skip(num_elements_to_skip);
-	generate(begin(elements_to_skip), end(elements_to_skip), gen);
+	std::generate(begin(elements_to_skip), end(elements_to_skip), gen);
 
 	size_t lut_size = size / 4;
 
@@ -110,19 +147,32 @@ generate_sinewave(
 		lut.push_back((int)roundf(0x7FFF * sinf(2.0 * M_PI * i / lut_size)));
 	}
 
+	double min = DBL_MAX;
+	double max = -DBL_MAX;
 	for (int i = 0; i < signed(size / 2); ++i) {
 		int val = double(lut[(int)phase]);
+		if (val > max) {
+			max = val;
+		}
+		if (val < min) {
+			min = val;
+		}
 		_tone_single_channel[i] = val;
 		phase += delta_phi;
 		if (phase >= lut_size)
 			phase -= lut_size;
 	}
 
+	std::uniform_real_distribution<double> amplitude_dist(min, max);
+	auto amplitude_gen = [&amplitude_dist, &mersenne_engine]() {
+		return amplitude_dist(mersenne_engine);
+	};
+
 	std::vector<double> tone_single_channel(
 	    _tone_single_channel, _tone_single_channel + size / 2);
 
 	for (auto skip : elements_to_skip) {
-		tone_single_channel.at(skip) = 0;
+		tone_single_channel.at(skip) = amplitude_gen();
 	}
 
 	return tone_single_channel;
