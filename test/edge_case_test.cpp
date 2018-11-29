c4b69e2fc5afd184a135c7c195fdbcda63f7a347
Sevag Hanssian <sevag.hanssian@gmail.com>
2018-11-28 18:29:32 -0800

    Add basic docs

--- test/edge_case_test.cpp
+++ test/edge_case_test.cpp
@@ -3,8 +3,7 @@
 
 TEST(MpmEdgeCase, EmptyData)
 {
-	EXPECT_THROW(
-	    pitch::mpm(std::vector<double>(), 44100), std::invalid_argument);
+	EXPECT_THROW(pitch::mpm(std::vector<double>(), 44100), std::bad_alloc);
 }
 
 TEST(MpmEdgeCase, SmallData)
@@ -13,21 +12,24 @@ TEST(MpmEdgeCase, SmallData)
 	ASSERT_EQ(-1.0, pitch);
 }
 
-TEST(MpmEdgeCase, EmptyAlloc)
+TEST(MpmEdgeCase, InvalidAlloc)
 {
 	EXPECT_THROW(MpmAlloc ma(-1), std::bad_alloc);
 }
 
+TEST(MpmEdgeCase, EmptyAlloc)
+{
+	EXPECT_THROW(MpmAlloc ma(0), std::bad_alloc);
+}
+
 TEST(YinEdgeCase, EmptyData)
 {
-	EXPECT_THROW(
-	    pitch::mpm(std::vector<double>(), 44100), std::invalid_argument);
+	EXPECT_THROW(pitch::mpm(std::vector<double>(), 44100), std::bad_alloc);
 }
 
 TEST(YinEdgeCase, TooSmallData)
 {
-	EXPECT_THROW(
-	    pitch::yin(std::vector<double>(1), 44100), std::invalid_argument);
+	EXPECT_THROW(pitch::yin(std::vector<double>(1), 44100), std::bad_alloc);
 }
 
 TEST(YinEdgeCase, SmallData)
@@ -35,7 +37,12 @@ TEST(YinEdgeCase, SmallData)
 	EXPECT_NO_THROW(pitch::yin(std::vector<double>(2), 44100));
 }
 
-TEST(YinEdgeCase, EmptyAlloc)
+TEST(YinEdgeCase, InvalidAlloc)
 {
 	EXPECT_THROW(YinAlloc ya(-1), std::bad_alloc);
+}
+
+TEST(YinEdgeCase, EmptyAlloc)
+{
+	EXPECT_THROW(YinAlloc ma(0), std::bad_alloc);
 }
